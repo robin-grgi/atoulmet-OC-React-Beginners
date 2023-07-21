616c08a190fd973c56ce16d0334cb2b1443806be
robin-grgi <georgi.robin@gmail.com>
2023-07-21 14:26:22 +0200

    Update ShoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,11 +2,28 @@ import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
 	return (
-		<ul>
-			{plantList.map((plant) => (
-				<li key={plant.id}>{plant.name} ({plant.category})</li>
-			))}
-		</ul>
+		<div>
+			<ul>
+				{
+					plantList.map((plant) => (plant.category)).reduce((accumulator, currentValue) => {
+						if (accumulator.includes(currentValue)) {
+							return accumulator
+						} else {
+							return [...accumulator, currentValue]
+						}
+					}
+						, []).map((category) => (
+							<li key={category}>{category}</li>
+						))
+				}
+			</ul>
+
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name} ({plant.category})</li>
+				))}
+			</ul>
+		</div>
 	)
 }
 
