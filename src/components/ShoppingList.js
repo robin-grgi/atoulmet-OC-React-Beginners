e2dacce14c6f503158bba8ec71f24d581b940f63
robin-grgi <georgi.robin@gmail.com>
2023-07-21 12:11:20 +0200

    Update ShoppingList.js

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,13 @@
 import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
-	return <ul></ul>
+	return (
+		<ul>
+			{plantList.map((plant) => (
+				<li key={plant.id}>{plant.name} ({plant.category})</li>
+			))}
+		</ul>
+	)
 }
 
 export default ShoppingList
